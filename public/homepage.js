20a09a9a95f3931995660e98c81ddfc07d7850c7
csmpls <yes@cosmopol.is>
2015-01-10 21:10:27 +0000

    cleaned up

--- public/homepage.js
+++ public/homepage.js
@@ -1,5 +1,3 @@
-
-
 function joinChatroom() {
        $('.alert').css('display','none');
 	var chatroomName =$("#chatroomName").val();
@@ -16,3 +14,24 @@ $(document).keydown(function(e){
 	joinChatroom();
 	}
 });
+
+
+
+$(document).ready(function(){
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+});
+
+
